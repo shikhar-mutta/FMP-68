ab04e909267d99cec079270f243a47e9ba8db931
Shikhar Mutta <shikharmutta67@gmail.com>
2026-04-15 17:00:40 +0530

    U50: Add Jest test framework and scripts to backend

---
+++ backend/jest.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: 'src',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: [
+    '**/*.(t|j)s',
+    '!**/*.module.ts',
+    '!**/main.ts',
+    '!**/index.ts',
+  ],
+  coverageDirectory: '../coverage',
+  testEnvironment: 'node',
+  roots: ['<rootDir>'],
+  moduleNameMapper: {
+    '^src/(.*)$': '<rootDir>/$1',
+  },
+  collectCoverage: true,
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+};
